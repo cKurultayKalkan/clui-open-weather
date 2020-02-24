903bd6abb44f854f27bc9c0d81e5d2f28473dd52
Çağdaş Kurultay Kalkan <me@Cagdas-MacBook-Pro.local>
2020-02-24 21:59:05 +0300

    try to use cluei

--- cmds/help.js
+++ cmds/help.js
@@ -1,6 +1,27 @@
 const chalk = require('chalk');
+const CLI = require('clui');
+const clc = require('cli-color');
 
 const { log } = console;
+
+const { Line, LineBuffer } = CLI;
+
+const outputBuffer = new LineBuffer({
+  x: 0,
+  y: 0,
+});
+
+new Line(outputBuffer)
+  .column('---------#   Help For Havalar Nasıl   #---------', 100, [clc.green])
+  .fill()
+  .store();
+
+new Line(outputBuffer)
+  .column('Presentation of colorful manual', 100, [clc.red])
+  .fill()
+  .store();
+
+outputBuffer.output();
 
 const menus = {
   main: `
@@ -25,6 +46,5 @@ module.exports = (args) => {
   const subCmd = args._[0] === 'help'
     ? args._[1]
     : args._[0];
-
   log(menus[subCmd] || chalk.blue(menus.main));
 };
